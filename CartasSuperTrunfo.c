74c644663f436996ac3974168c06130d829e50ac
juliana estrela <julianalekaseduarda@gmail.com>
2025-11-14 13:16:02 -0300

    Fix input prompts and output formatting in CartasSuperTrunfo
    Corrected formatting and variable names in user prompts and outputs.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,43 +5,42 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // char grupo;
-  int numero;
-  char nomePais[50];
-  long int populacao;
-  float area 
-  float pib
-  int pontosturisticos;
+  // int numero; 
+     char nomePais[50];
+     long int populacao;
+     float area;
+     float pib;
+     int pontosturisticos;
   
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
-    printf(¨Digite o nome do país; ¨);
+    printf(¨Digite o nome do pais; ¨);
     scanf(¨%s,nomePais);
-    
-    printf(¨qDigite o número da carta:¨);
-    
+  //
+      
+    printf(¨Digite o numero da carta:¨);
     scanf(¨%d¨, &numero);
 
-    printf(¨Digite a população: ¨);
-    scanf(¨%1d¨, &populacao);
+    printf(¨Digite a populacao: ¨);
+    scanf (¨%1d¨, &populacao);
       
-    printf(¨Digite a área em km²: ¨);
-    scanf(¨%f¨, &area);
+    printf(¨Digite a area em km²: ¨);
+    scanf (¨%f¨, &area);
 
     printf(¨Digite o PIB: ¨);
-    scanf(¨%f¨, &pib);
+    scanf (¨%f¨, &pib);
 
-    printf(¨Digite o número de pontos turísticos: ¨);
-    scanf(¨%d¨, &pontosturisticos);
+    printf(¨Digite o numero de pontos turisticos: ¨);
+    scanf (¨%d¨, &pontosturisticos);
 
-  // Área para exibição dos dados da cidade
-      printf(¨\n--- Dados da Carta ---\n¨);
-      printf(¨Nome do país; %s\n¨, nomedoPais);
-      printf(¨número da carta: %d\n¨, numero);
+  // Area para exibicao dos dados da cidade
+      printf(¨\n-- Dados da Carta --\n¨);
+      printf(¨Nome do pais; %s\n¨, nomedoPais);
+      printf(¨numero da carta: %d\n¨, numero);
       printf(¨População: %ld habitantes\n¨, populacao);
-      printf(¨Área: %.2f km²\n¨, area);
-      printf(¨Pontos turísticos: %d\n¨, pontosturisticos);
+      printf(¨Area: %.2f km²\n¨, area);
+      printf(¨Pontos turisticos: %d\n¨, pontosturisticos);
 
-return 0;
-} 
+      return 0;
+      } 
